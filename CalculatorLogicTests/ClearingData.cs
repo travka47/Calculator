00fc5525a71fa8ead8bcef391de7133f444e52bc
travka47 <travka.eal@students.dvfu.ru>
2022-06-29 22:32:14 +1000

    Tests, more tests

--- CalculatorLogicTests/ClearingData.cs
+++ CalculatorLogicTests/ClearingData.cs
@@ -1,10 +1,51 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using Calculator.Controllers;
+using Calculator.Models;
+using NUnit.Framework;
 
 namespace CalculatorLogicTests
 {
     [TestFixture]
     public class ClearingData
     {
+        private State _state;
         
+        [SetUp]
+        public void Setup()
+        {
+            _state = new State { Input = "0", History = new Stack<string>() };
+        }
+
+        [Test]
+        public void ClearInput()
+        {
+            CalculatorController.Dispatch(ref _state,"5");
+            CalculatorController.Dispatch(ref _state,"+");
+            Assert.AreEqual(5, _state.LeftOperand);
+            
+            CalculatorController.Dispatch(ref _state,"6");
+            Assert.AreEqual("6", _state.Input);
+            
+            CalculatorController.Dispatch(ref _state, "CE");
+            Assert.IsNotNull(_state.Input);
+            Assert.IsNotNull(_state.LeftOperand);
+            Assert.AreEqual("0", _state.Input);
+            Assert.AreEqual(5, _state.LeftOperand);
+        }
+
+        [Test]
+        public void ClearAllData()
+        {
+            CalculatorController.Dispatch(ref _state,"5");
+            CalculatorController.Dispatch(ref _state, "+");
+            Assert.AreEqual(5, _state.LeftOperand);
+            
+            CalculatorController.Dispatch(ref _state, "6");
+            Assert.AreEqual("6", _state.Input);
+            
+            CalculatorController.Dispatch(ref _state,"C");
+            Assert.AreEqual("0", _state.Input);
+            Assert.AreEqual(0, _state.History.Count);
+        }
     }
 }
--- CalculatorLogicTests/UnaryOperations.cs
+++ CalculatorLogicTests/UnaryOperations.cs
@@ -1,10 +1,85 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using Calculator.Controllers;
+using Calculator.Models;
+using NUnit.Framework;
 
 namespace CalculatorLogicTests
 {
     [TestFixture]
     public class UnaryOperations
     {
-        
+        private State _state;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _state = new State{Input = "0", History = new Stack<string>()};
+        }
+
+        [Test]
+        [TestCase("1", "1")]
+        [TestCase("5", "0,2")]
+        public void Reciprocal(string num, string expected)
+        {
+            CalculatorController.Dispatch(ref _state, num);
+            CalculatorController.Dispatch(ref _state, "1/x");
+
+            Assert.AreEqual(expected, _state.Input);
+        }
+
+        [Test]
+        [TestCase("9", "3")]
+        [TestCase("4", "2")]
+        [TestCase("5", "2,23606797749979")]
+        public void Sqrt(string num, string expected)
+        {
+            CalculatorController.Dispatch(ref _state, num);
+            CalculatorController.Dispatch(ref _state, "√");
+            
+            Assert.AreEqual(expected, _state.Input);
+        }
+
+        [Test]
+        [TestCase("5", "-5")]
+        public void Negate(string num, string expected)
+        {
+            CalculatorController.Dispatch(ref _state, num);
+            CalculatorController.Dispatch(ref _state, "±");
+           
+            Assert.AreEqual(expected, _state.Input);
+        }
+
+        [Test]
+        public void GetPercentWithoutLeftOperand()
+        {
+            CalculatorController.Dispatch(ref _state, "5");
+            CalculatorController.Dispatch(ref _state, "%");
+            
+            Assert.AreEqual("0", _state.Input);
+        }
+
+        [Test]
+        public void GetPercentWithRightOperand()
+        {
+            CalculatorController.Dispatch(ref _state, "1");
+            CalculatorController.Dispatch(ref _state, "0");
+            CalculatorController.Dispatch(ref _state, "0");
+            CalculatorController.Dispatch(ref _state, "+");
+            CalculatorController.Dispatch(ref _state, "1");
+            CalculatorController.Dispatch(ref _state, "0");
+            CalculatorController.Dispatch(ref _state, "%");
+            
+            Assert.AreEqual("10", _state.Input);
+        }
+
+        [Test]
+        public void InputAfterOperation()
+        {
+            CalculatorController.Dispatch(ref _state, "4");
+            CalculatorController.Dispatch(ref _state, "√");
+            CalculatorController.Dispatch(ref _state, "1");
+            
+            Assert.AreEqual("1", _state.Input);
+        } 
     }
 }
